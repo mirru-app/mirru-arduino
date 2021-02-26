c25edc704e1b7b7bacfd253d1fc6a5ab0b2812ce
Violet Forest <info@violetforest.com>
2021-02-26 18:48:06 +0100

    analog read

--- Bluetooth-Arduino-Prosthetic-Control.ino
+++ Bluetooth-Arduino-Prosthetic-Control.ino
@@ -5,16 +5,17 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 #include "HandServos.h"
-#include "MyoSensor.h"
 
 HandServos handServos;
-MyoSensor myoSensor(A0);
 
 bool deviceConnected = false;
 BLECharacteristic *pCharacteristic;       
 
 #define SERVICE_UUID           "0000180d-b5a3-f393-e0a9-e50e24dcca9e"
 #define CHARACTERISTIC_UUID_RX "00002a37-b5a3-f393-e0a9-e50e24dcca9e"
+
+long interval;
+unsigned long previousMillis;
 
 class OnConnectCallback: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -33,14 +34,17 @@ class ReceivedDataCallback: public BLECharacteristicCallbacks {
       std::string rxValue = pCharacteristic->getValue();
       String rxValueString = pCharacteristic->getValue().c_str();
       handServos.moveServos(rxValueString);
+      Serial.println(analogRead(A2));
     }
 };
 
-void setup(void) {
-  Serial.begin(9600);
-  pinMode(13, OUTPUT);
-
+void setup() {
+  Serial.begin(115200);
+  interval = 5;
+  previousMillis = 0;
+  
   handServos.setupServos();
+  
   BLEDevice::init("Brunel Hand");
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new OnConnectCallback());
@@ -60,5 +64,8 @@ void setup(void) {
 }
 
 void loop() {
-  myoSensor.readValues();
+  Serial.println(analogRead(A2));
+  delay(20);
+  Serial.println(analogRead(A3));
+  delay(20);
 }
